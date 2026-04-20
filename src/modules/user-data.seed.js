72cd9795f103dae8c56616eb9b29aea798064d3a
Joy <joykenneth605@gmail.com>
2026-04-20 23:00:50 +0100

    perf: add require logger module to seeder file

--- src/modules/user-data.seed.js
+++ src/modules/user-data.seed.js
@@ -2,6 +2,7 @@ const { uuidv7 } = require("uuidv7");
 const data = require("../seed/seed_profiles.json");
 const pool = require("../startup/database");
 const winston = require("winston");
+require("../startup/logger")();
 
 const seed = async () => {
   try {
