35875b598953239ac2d76ecf243fb73ece90f9c3
Joy <joykenneth605@gmail.com>
2026-04-20 22:43:07 +0100

    fix: add connection success and error to db connection

--- src/startup/database.js
+++ src/startup/database.js
@@ -1,8 +1,18 @@
 const { Pool } = require("pg");
 const config = require("../config");
+const winston = require("winston");
 
 const pool = new Pool({
     connectionString: config.POSTGRES_URI,
+});
+
+pool.on("error", (err) => {
+    winston.error("Unexpected error on idle client", err);
+    process.exit(-1);
+});
+
+pool.on("connect", () => {
+    winston.info("Database connection established");
 });
 
 module.exports = pool;
