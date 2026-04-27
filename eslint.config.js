453ad37d513901959abc891530d017f3a039927d
Joy <joykenneth605@gmail.com>
2026-04-27 14:22:54 +0100

    fix: fix eslint.config.js file

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,46 @@
-node_modules
-coverage
+const js = require("@eslint/js");
+
+module.exports = [
+    // files to ignore
+    {
+        ignores: [
+            "node_modules/**",
+            "dist/**",
+            "coverage/**"
+        ]
+    },
+
+    // your main config
+    {
+        ...js.configs.recommended,
+        languageOptions: {
+            ecmaVersion: "latest",
+            globals: {
+                // node globals
+                require: "readonly",
+                module: "readonly",
+                exports: "readonly",
+                process: "readonly",
+                __dirname: "readonly",
+                __filename: "readonly",
+                console: "readonly",
+
+                // jest globals
+                describe: "readonly",
+                it: "readonly",
+                test: "readonly",
+                expect: "readonly",
+                beforeAll: "readonly",
+                afterAll: "readonly",
+                beforeEach: "readonly",
+                afterEach: "readonly",
+                jest: "readonly"
+            }
+        },
+        rules: {
+            "no-unused-vars": "warn",
+            "no-console": "off",
+            "no-undef": "error"
+        }
+    }
+];
